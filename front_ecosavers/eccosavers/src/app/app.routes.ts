e100fdf9b5fb5170e6b5f018e0dfaf9e28200560
Zerritug <Fadeslol222@gmail.com>
2025-06-19 10:04:34 -0500

    Fixed header

--- front_ecosavers/eccosavers/src/app/app.routes.ts
+++ front_ecosavers/eccosavers/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
-import { PublicationsComponent } from '../pages/Publications/publications/publications.component';
+
 import { PersonComponent } from '../pages/Person/person/person.component';
 import { HomePageComponent } from '../pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-  { path: 'publications', component: PublicationsComponent },
   { path: 'person', component: PersonComponent },
   { path: 'home', component: HomePageComponent },
 
