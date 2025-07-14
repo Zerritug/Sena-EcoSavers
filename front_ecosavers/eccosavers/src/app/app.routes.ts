0c6d925b0f14898a93a9e467d432d7ebf88b8879
Andres <andysapsanty@gmail.com>
2025-07-14 13:53:36 -0500

    feat(login): Se añadieron y modificaron archivos dentro de la carpeta front_ecosavers - eccosavers los cambios fueron dentro de las subcarpetas src y pubic, dentro de public añadi varias imagenes que se usaron y usaran para el diseño de las interfaces, en la carpeta src se modifico el archivo main ts donde se insertaron las lineas de codigo desdela 4 hasta la 8, donde se importaron diferentes recursos a resaltar los formularios reactivos que se usaron, al añadir esto tambien se cambiaron algunas lineas en el bootsrap application en donde aparecen de nuevo algunos de los elementos importados, dentro de la subcarpeta app que esta dentro de src se modifico el archivo app.componet.html simplemente completado la etiqueta router-outlet, ya que no estaba completa anteriormente, tambien se modifico el archivo app.routes.ts donde se importo el componente de login page que se creo, dentro de pages cree la carpeta login-page que contiene sus respectivos archivos component, html, scss, y los 2 ts, dentro de la carpeta cree otras 2 login-footer y login-form para precisamente el footer y el formulario que se incorpora en el login, esos archivos, tambien contienen sus versiones de los archivos, html, css y los 2 ts, basicamente contiene los estilos y informacion del login, tambien se modifico el archivo shared, creando las carpetas models y service cada una con un archivo dentro de models, se encuentra el archivo que contiene una 4 lineas donde se establece las credneciales, username y password, dentro de services, se creo el archivo auth.services.ts que basicamente importa las credenciales y otros aspectos, esto sera importante para la logica y conexion con el backend y listo esos serian todos los cambios, incorporados en este primer commit, como resultado, ya se tiene  el apartado visual del login, aun no funcional

--- front_ecosavers/eccosavers/src/app/app.routes.ts
+++ front_ecosavers/eccosavers/src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PersonComponent } from '../pages/Person/person/person.component';
 import { HomePageComponent } from '../pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
+import { LoginPageComponent } from '../pages/login-page/login-page.component';
+
 
 export const routes: Routes = [
   { path: 'person', component: PersonComponent },
   { path: 'home', component: HomePageComponent },
+  { path: 'login',  component: LoginPageComponent },
 
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' },
