781ecf3091df1879cd33baf200ad8ada39858ab5
Andres <andysapsanty@gmail.com>
2025-07-23 17:35:10 -0500

    Se añadio el apartado de cambio de datos en la cuenta

--- front_ecosavers/eccosavers/src/app/app.routes.ts
+++ front_ecosavers/eccosavers/src/app/app.routes.ts
@@ -1,13 +1,25 @@
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PersonComponent } from '../pages/Person/person/person.component';
 import { HomePageComponent } from '../pages/home-page/home-page.component';
-import { NgModule } from '@angular/core';
+import { PersonComponent } from '../pages/Person/person/person.component';
+import { ProfileComponent } from '../pages/profile/profile.component';
 
+// Exporta la constante para que app.config.ts pueda importarla
 export const routes: Routes = [
-  { path: 'person', component: PersonComponent },
-  { path: 'home', component: HomePageComponent },
+  { path: 'home',    component: HomePageComponent },
+  { path: 'person',  component: PersonComponent },
+  { path: 'profile', component: ProfileComponent },
 
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' },
 ];
+
+@NgModule({
+  imports: [
+    // Puedes agregar opciones como initialNavigation si usas SSR
+    RouterModule.forRoot(routes /*, { initialNavigation: 'enabledBlocking' } */)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
